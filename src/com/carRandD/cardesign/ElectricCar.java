3953029228dc4e8980e95ead06287f120b517070
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 21:41:41 +0300

    getter ve setter eklendi

--- src/com/carRandD/cardesign/ElectricCar.java
+++ src/com/carRandD/cardesign/ElectricCar.java
@@ -9,4 +9,20 @@ public class ElectricCar extends CarSkeleton {
         this.avgKmPerCharge = avgKmPerCharge;
         this.batterySize = batterySize;
     }
+
+    public double getAvgKmPerCharge() {
+        return avgKmPerCharge;
+    }
+
+    public void setAvgKmPerCharge(double avgKmPerCharge) {
+        this.avgKmPerCharge = avgKmPerCharge;
+    }
+
+    public int getBatterySize() {
+        return batterySize;
+    }
+
+    public void setBatterySize(int batterySize) {
+        this.batterySize = batterySize;
+    }
 }
