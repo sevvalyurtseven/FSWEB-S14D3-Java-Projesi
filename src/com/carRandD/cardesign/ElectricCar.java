765939eb444e1e66509d50de1ca43d862efda12b
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 21:41:16 +0300

    constructor eklendi

--- src/com/carRandD/cardesign/ElectricCar.java
+++ src/com/carRandD/cardesign/ElectricCar.java
@@ -3,4 +3,10 @@ package com.carRandD.cardesign;
 public class ElectricCar extends CarSkeleton {
     private double avgKmPerCharge;
     private int batterySize;
+
+    public ElectricCar(String name, String description, double avgKmPerCharge, int batterySize) {
+        super(name, description);
+        this.avgKmPerCharge = avgKmPerCharge;
+        this.batterySize = batterySize;
+    }
 }
