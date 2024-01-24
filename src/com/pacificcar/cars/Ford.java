bae767ee6ba146bb4586081f5a9127e2f71a96f6
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 20:56:01 +0300

    car class'indaki methodlar override edildi

--- src/com/pacificcar/cars/Ford.java
+++ src/com/pacificcar/cars/Ford.java
@@ -4,4 +4,22 @@ public class Ford extends Car{
     public Ford(int cylinders, String name) {
         super(cylinders, name);
     }
+
+    @Override
+    public String startEngine() {
+       super.startEngine();
+       return getName() + " engine is starting";
+    }
+
+    @Override
+    public String accelerate() {
+        super.accelerate();
+        return getName() + "is accelerating";
+    }
+
+    @Override
+    public String brake() {
+         super.brake();
+         return getName() + " is braking";
+    }
 }
