979ea73232eadaf4f25b2520ac519c624f60eb5a
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 21:38:03 +0300

    getter ve setter eklendi

--- src/com/carRandD/cardesign/GasPoweredCar.java
+++ src/com/carRandD/cardesign/GasPoweredCar.java
@@ -9,4 +9,20 @@ public class GasPoweredCar extends CarSkeleton {
         this.avgKmPerLitre = avgKmPerLitre;
         this.cylinders = cylinders;
     }
+
+    public double getAvgKmPerLitre() {
+        return avgKmPerLitre;
+    }
+
+    public void setAvgKmPerLitre(double avgKmPerLitre) {
+        this.avgKmPerLitre = avgKmPerLitre;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
 }
