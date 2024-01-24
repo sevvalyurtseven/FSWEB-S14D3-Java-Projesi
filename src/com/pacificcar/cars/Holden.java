a96456f469730940008ffa4e20ec9d5b968cd57d
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 20:59:11 +0300

    car sinifindaki methodlar override edildi

--- src/com/pacificcar/cars/Holden.java
+++ src/com/pacificcar/cars/Holden.java
@@ -4,4 +4,21 @@ public class Holden extends Car{
     public Holden(int cylinders, String name) {
         super(cylinders, name);
     }
+    @Override
+    public String startEngine() {
+        super.startEngine();
+        return getName() + " engine is starting";
+    }
+
+    @Override
+    public String accelerate() {
+        super.accelerate();
+        return getName() + "is accelerating";
+    }
+
+    @Override
+    public String brake() {
+        super.brake();
+        return getName() + " is braking";
+    }
 }
