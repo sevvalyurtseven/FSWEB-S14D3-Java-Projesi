7831277702dfb26c1531e0231e261b2cefff45fb
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 21:25:58 +0300

    methodlar olusturuldu

--- src/com/carRandD/cardesign/CarSkeleton.java
+++ src/com/carRandD/cardesign/CarSkeleton.java
@@ -9,6 +9,20 @@ public class CarSkeleton {
         this.description = description;
     }
 
+    public String startEngine(){
+        System.out.println("Class Name: "+ getClass().getSimpleName());
+        return getName() + "starting engine";
+    }
+
+    public String drive(){
+        runEngine(this);
+        return getName()+ " is driving.";
+    }
+
+    protected void runEngine(CarSkeleton carSkeleton){
+        System.out.println(carSkeleton.getClass().getSimpleName());
+    }
+
     public String getName() {
         return name;
     }
@@ -16,4 +30,5 @@ public class CarSkeleton {
     public String getDescription() {
         return description;
     }
+
 }
