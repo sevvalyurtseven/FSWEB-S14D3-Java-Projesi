f7d89ccaccef010322a893daecc0b72894ec73a6
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 20:58:23 +0300

    car sinifindaki methodlar override edildi

--- src/com/pacificcar/cars/Mitsubishi.java
+++ src/com/pacificcar/cars/Mitsubishi.java
@@ -4,4 +4,21 @@ public class Mitsubishi extends Car{
     public Mitsubishi(int cylinders, String name) {
         super(cylinders, name);
     }
+    @Override
+    public String startEngine() {
+        super.startEngine();
+        return getName() + " engine is starting";
+    }
+
+    @Override
+    public String accelerate() {
+        super.accelerate();
+        return getName() + "is accelerating";
+    }
+
+    @Override
+    public String brake() {
+        super.brake();
+        return getName() + " is braking";
+    }
 }
