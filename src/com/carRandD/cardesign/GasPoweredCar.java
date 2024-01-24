4b158a26deabf725e6e71ca2bfad534dee338ff8
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 21:37:41 +0300

    constructor olusturuldu

--- src/com/carRandD/cardesign/GasPoweredCar.java
+++ src/com/carRandD/cardesign/GasPoweredCar.java
@@ -3,4 +3,10 @@ package com.carRandD.cardesign;
 public class GasPoweredCar extends CarSkeleton {
     private double avgKmPerLitre;
     private int cylinders;
+
+    public GasPoweredCar(String name, String description, double avgKmPerLitre, int cylinders) {
+        super(name, description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.cylinders = cylinders;
+    }
 }
