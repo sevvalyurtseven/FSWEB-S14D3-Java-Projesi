15e59607f3139612bc98b5dd098e26110794db64
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 21:13:57 +0300

    constructor olusturuldu

--- src/com/carRandD/cardesign/CarSkeleton.java
+++ src/com/carRandD/cardesign/CarSkeleton.java
@@ -3,4 +3,9 @@ package com.carRandD.cardesign;
 public class CarSkeleton {
     private String name;
     private String description;
+
+    public CarSkeleton(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
