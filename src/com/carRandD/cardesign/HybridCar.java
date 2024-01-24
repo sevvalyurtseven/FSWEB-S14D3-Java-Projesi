9f28053a15438f3c860bdfdb786e66568e32cbd3
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 21:40:46 +0300

    getter ve setter eklendi

--- src/com/carRandD/cardesign/HybridCar.java
+++ src/com/carRandD/cardesign/HybridCar.java
@@ -11,4 +11,28 @@ public class HybridCar extends CarSkeleton {
         this.batterySize = batterySize;
         this.cylinders = cylinders;
     }
+
+    public double getAvgKmPerLitre() {
+        return avgKmPerLitre;
+    }
+
+    public void setAvgKmPerLitre(double avgKmPerLitre) {
+        this.avgKmPerLitre = avgKmPerLitre;
+    }
+
+    public int getBatterySize() {
+        return batterySize;
+    }
+
+    public void setBatterySize(int batterySize) {
+        this.batterySize = batterySize;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
 }
